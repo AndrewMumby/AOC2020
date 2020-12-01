1bf0ecc8a1227f30f257ac748de865ff8345b0f3
Andrew <Andrew@SKUMBY>
2020-12-01 16:10:08 +0000

    Add project files.

---
+++ AdventCoding2020/Day1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCoding2020
+{
+    class Day1
+    {
+        private static List<int> ParseInput(string input)
+        {
+            string[] inputList = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            List<int> numberList = new List<int>();
+            foreach (string inputItem in inputList)
+            {
+                numberList.Add(Int32.Parse(inputItem));
+            }
+            return numberList;
+        }
+        public static string A(string input)
+        {
+            List<int> numbers = ParseInput(input);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i+1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] + numbers[j] == 2020)
+                    {
+                        return (numbers[i] * numbers[j]).ToString();
+                    }
+                }
+            }
+            return "Error";
+        }
+        
+        public static string B(string input)
+        {
+            List<int> numbers = ParseInput(input);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    for (int k = j+1; k < numbers.Count; k++)
+                    if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                    {
+                        return (numbers[i] * numbers[j] * numbers[k]).ToString();
+                    }
+                }
+            }
+            return "Error";
+        }
+
+    }
+}
---
+++ AdventCoding2020/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCoding2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 25;
+            string part = "B";
+            for (day = 25; day > 0; day--)
+            {
+                Type searchDayClass = Type.GetType("AdventCoding2020.Day" + day);
+                if (searchDayClass != null)
+                {
+                    MethodInfo searchPartMethod = searchDayClass.GetMethod("B");
+                    if (searchPartMethod != null)
+                    {
+                        part = "B";
+                        break;
+                    }
+                    else
+                    {
+                        searchPartMethod = searchDayClass.GetMethod("A");
+                        if (searchPartMethod != null)
+                        {
+                            part = "A";
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Enter the day to run, or none for the latest (Day" + day + part + "):");
+            string input = Console.ReadLine();
+            if (input != "")
+            {
+
+                day = Convert.ToInt32(input.Substring(0, input.Length - 1));
+                part = input.Substring(input.Length-1).ToUpper();
+            }
+            Console.WriteLine("Running code for " + day + part);
+            Type dayClass = Type.GetType("AdventCoding2020.Day" + day);
+            if (dayClass != null)
+            {
+                MethodInfo partMethod = dayClass.GetMethod(part);
+                if (partMethod != null)
+                {
+                    // Got the method we're running
+                    // First, run the tests
+                    string[] testFileNames = Directory.GetFiles("Day" + day + "\\" + part);
+                    bool passed = true;
+                    foreach(string testFileName in testFileNames)
+                    {
+                        string[] testText = File.ReadAllLines(testFileName);
+                        string correctAnswer = testText[0];
+                        string question = "";
+                        for (int i = 1; i < testText.Length; i++)
+                        {
+                            question += testText[i] + Environment.NewLine;
+                        }
+                        question = question.Trim();
+                        string actualAnswer = partMethod.Invoke(dayClass, new object[] { question }).ToString();
+                        Console.Write("Test " + testFileName + ": ");
+                        if (actualAnswer.Equals(correctAnswer))
+                        {
+                            Console.WriteLine("PASSED");
+                        }
+                        else
+                        {
+                            Console.WriteLine("FAILED! Expected: " + correctAnswer + ", Actual: " + actualAnswer);
+                            passed = false;
+                        }
+                    }
+                    // If all the tests pass, run the problem text
+                    if (passed)
+                    {
+                        string problem = File.ReadAllText("Day" + day + "\\input.txt");
+                        Stopwatch sw = new Stopwatch();
+                        sw.Start();
+                        string answer = partMethod.Invoke(dayClass, new object[] { problem }).ToString();
+                        sw.Stop();
+                        Console.WriteLine("Answer: " + answer);
+                        Console.WriteLine("In: " + Convert.ToDouble(sw.ElapsedMilliseconds) / 1000 + "s");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tests failed. Aborting");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
