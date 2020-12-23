055d3ffd44849d2445378291f2e696b322044192
Andrew Mumby <Andrew@Mumby.info>
2020-12-23 11:51:24 +0000

    Refactored to use a looping linked list, with a dictionary for cup lookup. Now actually finishes in a realistic time

--- AdventCoding2020/Day23.cs
+++ AdventCoding2020/Day23.cs
@@ -9,94 +9,186 @@ namespace AdventCoding2020
     class Day23
     {
         static int maxCup;
+        static Dictionary<int, Cup> cupLookup;
+
+
         public static string A(string input)
         {
-            List<int> cups = ParseCups(input);
-            maxCup = cups.Max();
+            Cup firstCup = ProcessInput(input, 9);
+
             for (int i = 0; i < 100; i++)
             {
-                cups = PlayRound(cups);
+                firstCup = PlayRound(firstCup);
             }
-            return GetLabels(cups).ToString();
 
+            // find the 1
+            Cup currentCup = cupLookup[1];
+
+            StringBuilder returnString = new StringBuilder();
+            do
+            {
+                currentCup = currentCup.NextCup;
+                returnString.Append(currentCup.CupLabel);
+            } while (currentCup.CupLabel != 1);
+
+            returnString.Remove(returnString.Length - 1, 1);
+            return returnString.ToString();
         }
 
         public static string B(string input)
         {
-            List<int> cups = ParseCups(input);
-            for (int i = cups.Max()+1; i <= 1000000; i++)
-            {
-                cups.Add(i);
-            }
-            maxCup = cups.Max();
+            Cup firstCup = ProcessInput(input, 1000000);
             for (int i = 0; i < 10000000; i++)
             {
-                cups = PlayRound(cups);
-                if (i %10 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                firstCup = PlayRound(firstCup);
             }
 
-            int pos1 = cups.IndexOf(1);
-            return (cups[pos1 + 1] * cups[pos1 + 2]).ToString();
-        }
-
-        private static long GetLabels(List<int> cups)
-        {
-            long answer = 0;
             // find the 1
-            int onePos = cups.IndexOf(1);
-            int i = onePos + 1;
-            while (i < cups.Count)
-            {
-                answer *= 10;
-                answer += cups[i];
-                i++;
-            }
-            i = 0;
-            while (i < onePos)
-            {
-                answer *= 10;
-                answer += cups[i];
-                i++;
-            }
-            return answer;
+
+            Cup currentCup = cupLookup[1];
+            return ((long)currentCup.NextCup.CupLabel * (long)currentCup.NextCup.NextCup.CupLabel).ToString();
+
         }
 
-        private static List<int> ParseCups(string input)
+        private static Cup ProcessInput(string input, int cupCount)
         {
-            return input.Select(c => c - '0').ToList();
-        }
-
-        private static List<int> PlayRound(List<int> cups)
-        {
-            // get current cup
-            int currentCup = cups[0];
-
-            //get removed cups
-            List<int> removedCups = cups.GetRange(1, 3);
-
-            // select destination cup
-            int destinationCup = (currentCup - 1);
-            while (destinationCup <= 0 || removedCups.Contains(destinationCup))
+            cupLookup = new Dictionary<int, Cup>();
+            Cup firstCup = new Cup(input[0] - '0', null);
+            cupLookup.Add(firstCup.CupLabel, firstCup);
+            Cup newCup = firstCup;
+            for (int i = 1; i < cupCount; i++)
             {
-                destinationCup--;
-                if (destinationCup <= 0)
+                if (i < input.Length)
                 {
-                    destinationCup = maxCup;
+                    newCup = new Cup(input[i] - '0', newCup);
+                    cupLookup.Add(newCup.CupLabel, newCup);
+                    maxCup = Math.Max(maxCup, input[i] - '0');
+                }
+                else
+                {
+                    newCup = new Cup(i + 1, newCup);
+                    cupLookup.Add(newCup.CupLabel, newCup);
+                    maxCup = i+1;
+                }
+            }
+            firstCup.PreviousCup = newCup;
+            newCup.NextCup = firstCup;
+            return firstCup;
+        }
+
+        private static Cup PlayRound(Cup firstCup)
+        {
+            Cup currentCup = firstCup;
+            Cup startRemove = currentCup.NextCup;
+            Cup endRemove = startRemove.NextCup.NextCup;
+            int labelToFind = currentCup.CupLabel - 1;
+            if (labelToFind < 1)
+            {
+                labelToFind = maxCup;
+            }
+
+            while (labelToFind == startRemove.CupLabel || labelToFind == startRemove.NextCup.CupLabel || labelToFind == endRemove.CupLabel)
+            {
+                labelToFind--;
+                if (labelToFind <1)
+                {
+                    labelToFind = maxCup;
                 }
             }
 
-            // new order : <4 - destinationCupPos-1> <destinationCup> <removedCups> <destinationCupPos+1 - end> <currentCup>
-            int destinationCupPos = cups.IndexOf(destinationCup);
-            List<int> newCups = new List<int>(10000000);
-            newCups.AddRange(cups.GetRange(4, destinationCupPos - 4));
-            newCups.Add(destinationCup);
-            newCups.AddRange(removedCups);
-            newCups.AddRange(cups.GetRange(destinationCupPos + 1, cups.Count - (destinationCupPos + 1)));
-            newCups.Add(currentCup);
-            return newCups;
+            Cup destinationCup = cupLookup[labelToFind];
+
+            Cup newFirstCup = endRemove.NextCup;
+
+            // fix the new loop, removing the removed cups
+            newFirstCup.PreviousCup = firstCup;
+            firstCup.NextCup = newFirstCup;
+
+            // insert the removed Cups
+            Cup postDestinationCup = destinationCup.NextCup;
+            destinationCup.NextCup = startRemove;
+            startRemove.PreviousCup = destinationCup;
+            endRemove.NextCup = postDestinationCup;
+            postDestinationCup.PreviousCup = endRemove;
+
+            return newFirstCup;
         }
     }
+
+    class Cup
+    {
+        Cup previousCup;
+        Cup nextCup;
+        int cupLabel;
+
+        internal Cup NextCup
+        {
+            get
+            {
+                return nextCup;
+            }
+
+            set
+            {
+                nextCup = value;
+            }
+        }
+
+        internal Cup PreviousCup
+        {
+            get
+            {
+                return previousCup;
+            }
+
+            set
+            {
+                previousCup = value;
+            }
+        }
+
+        public int CupLabel
+        {
+            get
+            {
+                return cupLabel;
+            }
+        }
+
+        public Cup(int label, Cup previous)
+        {
+            this.cupLabel = label;
+            this.previousCup = previous;
+            if (previous != null)
+            {
+                previous.NextCup = this;
+            }
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            { 
+                return false;
+            }
+
+            // TODO: write your implementation of Equals() here
+            return this.cupLabel == ((Cup)obj).cupLabel;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            return this.cupLabel.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return cupLabel.ToString();
+        }
+
+    }
+    
 }
