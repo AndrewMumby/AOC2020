c475cdee7d342a39924edef64c58c2f73907111b
Andrew Mumby <Andrew@Mumby.info>
2020-12-24 13:19:13 +0000

    13B now actually does the calculating, rather than outsourcing it to Wolfram Alpha

--- AdventCoding2020/Day13.cs
+++ AdventCoding2020/Day13.cs
@@ -43,17 +43,58 @@ namespace AdventCoding2020
             List<int> BusIds = new List<int>();
             List<string> busStringList = inputList[1].Split(new char[] { ',' }).ToList();
             int count = 0;
-            string output = "";
+            List<Tuple<int, int>> busses = new List<Tuple<int, int>>();
+
             for (int i = 0; i < busStringList.Count; i++)
             {
                 if (!busStringList[i].Equals("x"))
                 {
-                    //Console.WriteLine("Bus ID " + busStringList[i] + " needs to arrive " + i + " minutes after t");
-                    output = output  + "t + " + i + " mod " + busStringList[i] + " = 0, ";
-                        count++;
+                    busses.Add(Tuple.Create(Convert.ToInt32(busStringList[i]), i));
+                    count++;
                 }
             }
-            return output.Substring(0, output.Length - 2);
+            int busNo = 0;
+            long interval = 1;
+            long answer = 0;
+            while (busNo < busses.Count-1)
+            {
+                long firstNumber = 0;
+                long secondNumber = 0;
+                Tuple<int, int> testBus = busses[busNo];
+                while (firstNumber == 0)
+                {
+                    if (TestBus(answer, testBus))
+                    {
+                        firstNumber = answer;
+                    }
+                    answer += interval;
+                }
+                while (secondNumber == 0)
+                {
+                    if (TestBus(answer, testBus))
+                    {
+                        secondNumber = answer;
+                    }
+                    answer += interval;
+                }
+                answer -= interval;
+                interval = interval * testBus.Item1;
+                busNo++;
+            }
+
+            while (true)
+            {
+                if (TestBus(answer, busses.Last()))
+                {
+                    return answer.ToString();
+                }
+                answer += interval;
+            }
+        }
+
+        private static bool TestBus(long time, Tuple<int, int> bus)
+        {
+            return ((time + bus.Item2) % bus.Item1 == 0);
         }
 
     }
